1ae7e0ca8bef6f98bdd525733e176176e0215097
EmixMaxime <emixmaxime@outlook.fr>
2016-09-26 18:51:25 +0200

    Rename folder typescript to javascript

--- build/config.js
+++ build/config.js
@@ -6,7 +6,7 @@ var publicDirectory = 'public'
 module.exports = {
     publicDirectory: 'public',
     entry: {
-        app: ['./src/sass/index.scss', './src/typescript/main.js', './src/app.js']
+        app: ['./src/sass/index.scss', './src/javascript/main.js']
     },
     output: {
         path: path.resolve(root, publicDirectory), // attention ce dossier sera vidé systématiquement (ne rien y mettre !)
