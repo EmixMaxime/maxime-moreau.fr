d08dac25b4c163aa58004ba669217baa93850974
EmixMaxime <emixmaxime@outlook.fr>
2017-04-18 15:40:41 +0200

    Adding an observable object

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+// Petit polyfill
+import './advantages';
+
+if (NodeList.prototype.forEach === undefined) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+const chart = document.querySelector('#chart');
+const batons = chart.querySelectorAll('[data-percentage]');
+
+batons.forEach(elm => {
+  const h = (elm.dataset.percentage / 100) * chart.clientHeight;
+  console.log(h);
+  elm.style.height = `${h}px`;
+});
+
+// Observable
+
+const opts = {
+  threshold: [0.7],
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+
+    if (entry.intersectionRatio > 0.7) {
+      entry.target.classList.add('observable--visible');
+      observer.unobserve(entry.target);
+      console.log('section visible');
+    }
+
+  });
+}, opts);
+
+const observables = document.querySelectorAll('.observe');
+observables.forEach(observable => observer.observe(observable));
