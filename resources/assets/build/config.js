5b0e883438db37495fb7b0a288c810a8026e7830
EmixMaxime <emixmaxime@outlook.fr>
2017-04-19 22:31:43 +0200

    Fix build

--- resources/assets/build/config.js
+++ resources/assets/build/config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
+const buildPath = path.resolve('./public/assets/');
+console.log(buildPath);
+
 module.exports = {
   entry: {
     main: ['./resources/assets/sass/main.scss', './resources/assets/js/main.js'],
@@ -6,7 +11,7 @@ module.exports = {
   html: false,
   browsers: ['last 2 versions', 'ie > 8'],
   assets_url: '/assets/',  // Urls dans le fichier final attention au / à la fin pour la génération du fichier assets.json !!
-  assets_path: './public/assets/', // ou build ? : Attention -> obligation de mettre le / à  la fin
+  assets_path: `${buildPath}/`, // ou build ? : Attention -> obligation de mettre le / à  la fin
   stylelint: '',
   refresh: ['./resources/views/**/*.twig'], // Permet de forcer le rafraichissement du navigateur lors de la modification de ces fichiers
   historyApiFallback: false // Passer à true si on utilise le mode: 'history' de vue-router (redirige toutes les requêtes sans réponse vers index.html)
