e6bebf3d198c2e358aaadeedb7759a65e267d2ec
EmixMaxime <emixmaxime@outlook.fr>
2017-04-20 12:42:43 +0200

    Sticky bar is only for desktop menu

--- resources/assets/js/topbar.js
+++ resources/assets/js/topbar.js
@@ -1,19 +1,22 @@
-const throttle = require('lodash.throttle');
+// Les mobiles n'ont pas besoin de la sticky bar
+if (window.innerWidth > 600) {
+  const throttle = require('lodash.throttle');
 
-const getTop = elm => {
-  return elm.getBoundingClientRect().top
+  const getTop = elm => {
+    return elm.getBoundingClientRect().top
+  }
+
+  const topbar = document.querySelector('#topbar');
+
+  const stickyTobar = () => {
+    if (window.scrollY > 60) {
+      topbar.classList.add('topbar--stick');
+    }
+
+    if (window.scrollY == 0) {
+      topbar.classList.remove('topbar--stick');
+    }
+  };
+
+  window.addEventListener('scroll', throttle(stickyTobar, 200));
 }
-
-const topbar = document.querySelector('#topbar');
-
-const stickyTobar = () => {
-  if (window.scrollY > 60) {
-    topbar.classList.add('topbar--stick');
-  }
-
-  if (window.scrollY == 0) {
-    topbar.classList.remove('topbar--stick');
-  }
-};
-
-window.addEventListener('scroll', throttle(stickyTobar, 200));
