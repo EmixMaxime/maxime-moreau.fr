2ee322494a9d564086fda057f983b8a102605d7b
EmixMaxime <emixmaxime@outlook.fr>
2017-04-20 18:29:17 +0200

    Add scrollTo topbar animation

--- resources/assets/js/topbar.js
+++ resources/assets/js/topbar.js
@@ -1,3 +1,17 @@
+const scrollTo = require('scroll-to');
+
+const links = document.querySelectorAll('#topbar .topbar-link');
+
+links.forEach(function(link) {
+  const anchor = link.getAttribute('href');
+  const target = document.querySelector(anchor);
+  const y = target.getBoundingClientRect().top + window.scrollY;
+
+  link.addEventListener('click', function () {
+    scrollTo(0, y);
+  });
+});
+
 // Les mobiles n'ont pas besoin de la sticky bar
 if (window.innerWidth > 600) {
   const throttle = require('lodash/throttle');
