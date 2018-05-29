a14b211ff930e807dce44ed1e8fa44e6b7104915
EmixMaxime <emixmaxime@outlook.fr>
2018-05-29 16:42:09 +0200

    (trans) presentation

---
+++ resources/lang/fr_FR.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	// Presentation
+	"Bonjour, je m'appelle %name%" => "Hi, I'm %name%",
+	"étudiant en 2ème année de DUT Informatique en alternance." => "student",
+	"Apprenti développeur web à" => "Apprentice web developer at",
+	"Âge" => "Age",
+	"%age% ans" => "%age% years old",
+	"Secteur d'activité" => "Main activity",
+	"Développement web full-stack" => "Fullstack web development",
+
+];
