9222d22545232dd2cbda57c40a0ff5b37b31a344
EmixMaxime <emixmaxime@outlook.fr>
2018-05-29 19:05:50 +0200

    (trans) advantage

--- resources/lang/fr_FR.php
+++ resources/lang/fr_FR.php
@@ -15,5 +15,14 @@ return [
 	"veille technologique" => "",
 
 	"Depuis l'âge de 14 ans je m'intéresse au développement web. Depuis cet âge, je jongle entre mes cours scolaires et mon apprentissage autodidacte. Pendant que la plupart des adolescents s'amusaient sur des jeux vidéo, je m'amusais à apprendre comment créer un site web. Je ne cesse de développer et de m'autoformer, grâce à ce merveilleux outil qu'est Internet. Voici quelques sites où je me suis formé :" => "",
-	"Mon histoire" => "My little life story"
+	"Mon histoire" => "My little life story",
+
+	"Mes atouts" => "My assets",
+	"" => "",
+	"moi" => "me",
+	"Bon" => "good",
+	"Très bon" => "very good",
+	"Ambitieux" => "Ambitious",
+	"Sérieux" => "Serious",
+	"Curieux" => "Curious"
 ];
