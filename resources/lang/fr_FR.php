9a3a87ec8a062cd8b4b46b0bc86bdac3ff8b1c9c
EmixMaxime <emixmaxime@outlook.fr>
2018-05-29 16:47:58 +0200

    (wip trans) about

--- resources/lang/fr_FR.php
+++ resources/lang/fr_FR.php
@@ -10,4 +10,10 @@ return [
 	"Secteur d'activité" => "Main activity",
 	"Développement web full-stack" => "Fullstack web development",
 
+	"À propos de moi" => "About me",
+	"Ma personnalité" => "My personality",
+	"veille technologique" => "",
+
+	"Depuis l'âge de 14 ans je m'intéresse au développement web. Depuis cet âge, je jongle entre mes cours scolaires et mon apprentissage autodidacte. Pendant que la plupart des adolescents s'amusaient sur des jeux vidéo, je m'amusais à apprendre comment créer un site web. Je ne cesse de développer et de m'autoformer, grâce à ce merveilleux outil qu'est Internet. Voici quelques sites où je me suis formé :" => "",
+	"Mon histoire" => "My little life story"
 ];
