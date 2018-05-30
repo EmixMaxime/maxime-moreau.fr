7df8263cf3a3446231ae01b4095348e857888ac3
EmixMaxime <emixmaxime@outlook.fr>
2018-05-30 09:49:26 +0200

    (add) getData BaseController method

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,6 +4,7 @@ namespace App\http\Controllers;
 use Interop\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Views\Twig;
+use Emix\Support\PathHelpers;
 
 class BaseController
 {
@@ -18,16 +19,35 @@ class BaseController
 	 */
 	private $view;
 
+	/**
+	 * 
+	 */
+	protected $pathHelpers;
+
 
 	public function __construct (ContainerInterface $container)
 	{
 		$this->container = $container;
 		$this->view = $container->get('view');
+		$this->pathHelpers = $container->get(PathHelpers::class);
 	}
 
 	protected function render(ResponseInterface $response, $file, $data = []): ResponseInterface
 	{
 		return $this->view->render($response, $file, $data);
+	}
+
+	protected function getData(array $names): array
+	{
+		$dataPath = $this->pathHelpers->data;
+		$arr = [];
+
+		foreach (glob("${dataPath}/*.data.php") as $path) {
+			$filename = basename($path, '.data.php');
+			$arr[basename($filename)] = include($path);
+		}
+
+		return $arr;
 	}
 
 }
