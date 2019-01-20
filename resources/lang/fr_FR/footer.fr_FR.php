731a0241a9e446ae4aa436c3a789940d9497603f
EmixMaxime <emixmaxime@outlook.fr>
2019-01-20 11:55:39 +0100

    wip translates

---
+++ resources/lang/fr_FR/footer.fr_FR.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'Vous êtes intéressé par mon profil ? Une question ? Contactez-moi !' => 'Are you interested by me? A question? Feel free to contact me!',
+	"La passion du web par un jeune qui ne demande qu'à évoluer." => "",
+
+
+	'Retrouvez-moi' => 'Keep in touch',
+	'En bref' => 'In summary'
+];
---
+++ resources/lang/fr_FR/timeline.fr_FR.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+		// https://www.developpez.net/forums/d736648/emploi-etudes-informatique/emploi/cv/traduire-parcours-universitaire-anglais/
+		'2ème année DUT Informatique par alternance' => 'University Technology Diploma in Computer science by apprenticeship',
+
+];
